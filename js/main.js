a5aab6ce35a2fff5e046fe0825bf33ab03bf3e10
VladL <vlad169@yandex.ru>
2019-05-30 16:00:46 +0300

    примитивим через for in

--- js/main.js
+++ js/main.js
@@ -15,7 +15,15 @@ const renderProduct = (title, price) => {
 
 const renderPage = list => {
     const productsList = list.map(item => renderProduct(item.title, item.price));
-    document.querySelector('.products').innerHTML = productsList;
+    // document.querySelector('.products').innerHTML = productsList;
+    for(let prop in productsList){
+        console.log(productsList[prop]);
+        document.querySelector('.products').innerHTML += productsList[prop];
+    }
 };
+
+//map создает новый массив из элементов, в нашем случае новым массивом станет массив строк из renderProduct где строка будет содержать <div><h3>...<p>.... Только элемент массива с индексом 0 будет содержать первый товар, 1 второй товар и т.д. описанный полностью со всей разметкой. А так как productList тоже массив, его элементы разделяются запятыми, отсюда и вывод запятых через innerHTML. И это наглядней становится видно если заменить innerHTML на textContent - там так же будет текстом описана разметка и элементы разделены запятыми.
+
+//я попробовал сделать через перебор массива и вывод - но мне кажется это какой то примитивный способ :)
 
 renderPage(products);
