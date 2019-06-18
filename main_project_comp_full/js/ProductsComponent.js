8ea6fb9c0bf30e3c38302393e25e64e372511200
vlad <vlad169@yandex.ru>
2019-06-18 12:48:24 +0300

    сделан поиск

--- main_project_comp_full/js/ProductsComponent.js
+++ main_project_comp_full/js/ProductsComponent.js
@@ -8,8 +8,8 @@ Vue.component('products', {
       }
     },
     methods: {
-        filter(){
-            let regexp = new RegExp(this.userSearch, 'i');
+        filter(userSearch){
+            let regexp = new RegExp(userSearch, 'i');
             this.filtered = this.products.filter(el => regexp.test(el.product_name));
         }
     },
